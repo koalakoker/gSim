bfff36faad5a0af5f70d1cb52bd0bde5f5478331
koalakoker <koalakoker@gmail.com>
2016-07-30 15:54:22 +0200

    Implemented differen function

--- mathfunction.cpp
+++ mathfunction.cpp
@@ -9,5 +9,6 @@ mathFunction::mathFunction()
 double mathFunction::calc(double t)
 {
     double w = 2 * 3.14 * 3;
-    return sin(w*t);
+    double w2 = 2 * 3.14 * 8;
+    return sin(w*t) + (0.3 * sin(w2*t));
 }
