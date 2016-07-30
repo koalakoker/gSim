542d6ea22810d2d897a348a8428470e01fe07eb2
koalakoker <koalakoker@gmail.com>
2016-07-30 08:30:01 +0200

    Plotting example

--- wscope.cpp
+++ wscope.cpp
@@ -4,4 +4,14 @@
 WScope::WScope(QWidget *parent) : WOut(parent), ui(new Ui::WScope)
 {
     ui->setupUi(this);
+
+    ui->qplot->addGraph();
+    ui->qplot->graph(0)->setData(oscope.x, oscope.y);
+    // give the axes some labels:
+    ui->qplot->xAxis->setLabel("x");
+    ui->qplot->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->qplot->xAxis->setRange(-1, 1);
+    ui->qplot->yAxis->setRange(0, 1);
+    ui->qplot->replot();
 }
