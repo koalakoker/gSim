fb3c279130085265eda5ede9bf064d086435906d
koalakoker <koalakoker@gmail.com>
2016-08-27 07:08:40 +0200

    Data managment (SDataVector) for multiple initialization (d,d,d,..) done

--- simulation/sdatavector.cpp
+++ simulation/sdatavector.cpp
@@ -11,24 +11,6 @@ SDataVector::SDataVector(SData d)
     m_data.append(d);
 }
 
-SDataVector::SDataVector(double d) : m_data(QVector<SData>(1,SData(d)))
-{
-//    m_data.append(SData(d));
-}
-
-SDataVector::SDataVector(double d1, double d2) : m_data(QVector<SData>(1,SData(d1, d2)))
-{
-//    m_data.append(SData(d1, d2));
-}
-
-SDataVector::SDataVector(double d1, double d2, double d3) : m_data(QVector<SData>(1,SData(d1, d2, d3)))
-{
-//    SData data = SData(d1);
-//    data.append(d2);
-//    data.append(d3);
-//    m_data.append(data);
-}
-
 void SDataVector::setData(int slot, SData data)
 {
     m_data[slot] = data;
