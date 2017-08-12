eb1dfc2e432dc30bab34d489c84f5ed8d9be68b5
koalakoker <koalakoker@gmail.com>
2017-08-12 16:37:21 +0200

    Main updated with new tecqnique for simView (and Legacy)

--- main.cpp
+++ main.cpp
@@ -4,14 +4,31 @@
 
 #include "simTemplates/simModel/simModel9.h"    /* Update here for any new model */
 
+/****************** Legacy ******************/
+/*------------- Model 7 (SVM)  -------------*/
+//#include "simTemplates/simModel/simModel7.h"
+//#include "simTemplates/simView/simView7.h"
+/*----------- Model 8 (Resolver) -----------*/
+//#include "simTemplates/simModel/simModel8.h"
+//#include "simTemplates/simView/simView8.h"
+/********************************************/
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     gSim w;
     commonSimView v;
-
     simModel9 m;                                /* Update here for any new model */
+
+/****************** Legacy ******************/
+/*------------- Model 7 (SVM)  -------------*/
+//  simModel7 m;
+//  simView7 v;
+/*----------- Model 8 (Resolver) -----------*/
+//  simModel8 m;
+//  simView8 v;
+/********************************************/
 
     v.setSimModel(&m);
     v.updateView();
