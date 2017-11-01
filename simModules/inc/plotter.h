1a292adb5b47648bf7f08bf34111f079275327d6
koalakoker <koalakoker@gmail.com>
2017-11-01 08:22:37 +0100

    Working on fast plot (Source splitting, Line/Point style)

--- simModules/inc/plotter.h
+++ simModules/inc/plotter.h
@@ -8,44 +8,31 @@
 
 class Plotter : public QObject {
     Q_OBJECT
+
 public:
-    Plotter(QSize size, QRectF range, QVector<double> data)
-        : _size(size), _range(range), _data(data) { }
+    typedef enum {
+        POINT_STYLE,
+        LINE_STYLE
+    } style_t;
+
+    Plotter(QSize size, QRectF range, QVector<double> data, style_t style = LINE_STYLE)
+        : _size(size), _range(range), _data(data), m_style(style) { }
 
 signals:
     void done(QImage);
     void cleanup();
 
 public slots:
-    void plot() {
-        QElapsedTimer t;
-        t.start();
-        QImage img(_size, QImage::Format_ARGB32_Premultiplied);
-        img.fill(Qt::white);
-        QPainter p(&img);
-        QPen pen(Qt::darkBlue);
-        pen.setWidth(1);
-        p.setPen(pen);
-//        for (int i = 0; i < _data.size(); i += 2) {
-//            p.drawPoint(map(_data[i], _data[i + 1]));
-//        }
-        for (int i = 0; i < _data.size() - 2; i += 2) {
-            p.drawLine(map(_data[i], _data[i + 1]),map(_data[i+2], _data[i + 3]));
-        }
-        qDebug() << "plotted in" << t.elapsed() << "msec";
-        emit done(img);
-        emit cleanup();
-    }
+    void plot();
 
 private:
-    inline QPointF map(double x, double y) {
-        return QPointF(_size.width() * ((x * 1) / (_range.width() - _range.x())),
-                       (_size.height() * ((1-y) / (_range.height() - _range.y()))));
-    }
+    inline QPointF map(double x, double y);
 
     QSize _size;
     QRectF _range;
     QVector<double> _data;
+
+    style_t m_style;
 };
 
 #endif // PLOTTER_H
--- simModules/src/plotter.cpp
+++ simModules/src/plotter.cpp
@@ -1,3 +1,43 @@
 #include "plotter.h"
 
+void Plotter::plot()
+{
+    QElapsedTimer t;
+    t.start();
+    QImage img(_size, QImage::Format_ARGB32_Premultiplied);
+    img.fill(Qt::white);
+    QPainter p(&img);
+    QPen pen(Qt::darkBlue);
+    pen.setWidth(1);
+    p.setPen(pen);
+    switch (m_style)
+    {
+    case POINT_STYLE:
+        {
+            for (int i = 0; i < _data.size(); i += 2)
+            {
+                p.drawPoint(map(_data[i], _data[i + 1]));
+            }
+        }
+        break;
+    case LINE_STYLE:
+        {
+            for (int i = 0; i < _data.size() - 2; i += 2) {
+                p.drawLine(map(_data[i], _data[i + 1]), map(_data[i+2], _data[i + 3]));
+            }
+        }
+        break;
+    default:
+        break;
+    }
 
+    qDebug() << "plotted in" << t.elapsed() << "msec";
+    emit done(img);
+    emit cleanup();
+}
+
+inline QPointF Plotter::map(double x, double y)
+{
+    return QPointF(_size.width() * ((x * 1) / (_range.width() - _range.x())),
+                   (_size.height() * ((1-y) / (_range.height() - _range.y()))));
+}
